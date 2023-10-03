19d3bdac668c7f8980688de8a426652067adcd23
George <georgetenex@gmail.com>
2023-10-03 20:14:37 +0300

    first commit

---
+++ Eureka-service-discovery/src/main/java/com/tenex/Eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.tenex.Eurekaservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+	}
+
+}
